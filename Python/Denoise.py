febed56487c2f21bdb9a93b9f77a95f70a058769
Stuart Pittaway <1201909+stuartpittaway@users.noreply.github.com>
2022-01-25 15:39:11 +0000

    Update Denoise.py

--- Python/Denoise.py
+++ Python/Denoise.py
@@ -11,6 +11,7 @@ def Filelist(path: str, ext: str) -> int:
     return sorted(glob.glob(os.path.join(path, "frame_????????."+ext)), reverse=False)
 
 input_path = "E:\\source\\Super8FilmScanner\\Python\\Aligned"
+
 if not os.path.exists(input_path):
     raise FileNotFoundError("Missing input folder")
 
@@ -23,10 +24,14 @@ if len(files)==0:
     print("Nothing to do, so quit...")
     quit()
 
+
 frames=[]
-frame_number=0
+#Use first file name 
+frame_number=int(os.path.basename(files[0]).split('.')[0][6:])
 target_h=None
 target_w=None
+
+print("Starting frame number",frame_number)
 
 # These are automatically calculated if not supplied, based on the first image in the folder
 #target_w=2650
@@ -56,13 +61,13 @@ for filename in files:
             h, w =img.shape[:2]
 
 
-        # Resize image to Full HD 1920x1080 and put into 16:9 frame?
-        # Comment this out to leave at original resolution (note very slow!!)
-        if True==True:
+        # Resize image Full HD 1920x1080 and put into 16:9 frame?
+        # Comment this out to leave at original resolution (note very slow for high resolutions!!)
+        if True==False:
             output_w=1920
             output_h=1080
 
-            #Scale new_image to keep correct aspect ratio
+            # Scale new_image to keep correct aspect ratio
             scale = output_w/w
             if h*scale > output_h:
                 scale = output_h/h
@@ -71,15 +76,15 @@ for filename in files:
             scale_h=int(h*scale)
 
             print("Scaled image w=",scale_w,"h=",scale_h, "original w=",w,"h=",h)
-            #Horizontal centre frame
-            #scale_x_offset=int(output_w/2 - scale_w/2)
+            # Horizontal centre frame
+            # scale_x_offset=int(output_w/2 - scale_w/2)
             img=cv.resize(img.copy(), (scale_w,scale_h), interpolation=cv.INTER_AREA)
 
-            #Now place on a 1920x1080 frame
-            #new_image = np.zeros((output_h,output_w,3), np.uint8)
-            #new_image[0:scale_h,scale_x_offset:scale_x_offset+scale_w]=scaled_image
-            #img=new_image.copy()
-            #new_image=None
+            # Now place on a 1920x1080 frame
+            # new_image = np.zeros((output_h,output_w,3), np.uint8)
+            # new_image[0:scale_h,scale_x_offset:scale_x_offset+scale_w]=scaled_image
+            # img=new_image.copy()
+            # new_image=None
 
         # Debug resize to speed up processing
         #img = cv.resize(img, (0,0), fx=0.3, fy=0.3)
@@ -91,8 +96,8 @@ for filename in files:
         if len(frames)!=3:
             continue
 
-        #Our first output frame is number 1 (frame zero is skipped, as is the last one)
-        #This will renumber the frames if the input doesn't start at zero
+        # Our first output frame is number 1 (frame zero is skipped, as is the last one)
+        # This will renumber the frames if the input doesn't start at zero
         frame_number+=1
         output_filename = os.path.join(output_path, "frame_{:08d}.png".format(frame_number))
 
@@ -103,20 +108,18 @@ for filename in files:
             print("Processing...")
             
             # Frames has 0,1,2 image array
-            dst = cv.fastNlMeansDenoisingColoredMulti(frames, 1, 1, dst=None, h=3.5, hColor=3.5, templateWindowSize=7,searchWindowSize=21)
+            dst = cv.fastNlMeansDenoisingColoredMulti(frames, 1, 1, dst=None, h=3, hColor=3, templateWindowSize=7,searchWindowSize=29)
 
             if cv.imwrite(output_filename, dst, [cv.IMWRITE_PNG_COMPRESSION, 2])==False:
                 raise IOError("Failed to save image")
 
             cv.imshow("image_orig",cv.resize(frames[1], (0,0), fx=PREVIEW_SCALE, fy=PREVIEW_SCALE))
             cv.imshow("image_fixed",cv.resize(dst, (0,0), fx=PREVIEW_SCALE, fy=PREVIEW_SCALE))
-            #cv.imshow("image_orig",frames[1])
-            #cv.imshow("image_fixed",dst)
             cv.moveWindow("image_orig",0,80)
             cv.moveWindow("image_fixed",int(w*PREVIEW_SCALE),80)
 
             # Test for key and allow screen to refresh
-            k = cv.waitKey(250) & 0xFF
+            k = cv.waitKey(200) & 0xFF
 
             if k == 27:
                 break
@@ -124,6 +127,5 @@ for filename in files:
         # Remove oldest image
         frames.pop(0)
 
-
-#Exit
+# Exit
 cv.destroyAllWindows()
