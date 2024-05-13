fd587c06a01f29856dc6fadfa1a69d8cfc32e5c6
Níckolas Goline <nickolas.goline+github@gmail.com>
2024-05-13 14:04:10 -0300

    allow for frame dimesions adjusts

--- Python/ImageRegistrationCropping.py
+++ Python/ImageRegistrationCropping.py
@@ -22,6 +22,7 @@ max_y=0
 lower_t=225
 previous_frame_top_left_of_sproket_hole=None
 previous_frame_bottom_right_of_sproket_hole=None
+frame_dims=(0,0, 800, 540)
 
 def ServiceImageWriteQueue(q):
     while True:
@@ -235,13 +236,21 @@ def scanImages(files:List, maximum_number_of_samples:int=32):
 
     return average_sample_count,average_width,average_height,average_area
 
-def processImage(original_image, average_width, average_height, average_area):
+def resize_with_aspect(w, h, delta_w):
+    new_w = w + delta_w
+    ratio = new_w / w
+    new_h = h * ratio
+    return new_w, int(new_h), ratio
+
+def processImage(original_image, average_width, average_height, init_frame_dims):
     global min_x,max_x,min_y,max_y, lower_t
     global previous_frame_top_left_of_sproket_hole
     global previous_frame_bottom_right_of_sproket_hole
+    global frame_dims
 
     Detect=True
     manual_adjustment=False
+    first_frame=True
     #lower_t=210
     while True:        
         # Do inital crop of the input image
@@ -249,152 +258,140 @@ def processImage(original_image, average_width, average_height, average_area):
         image=cropOriginalImage(original_image)
         h, w =image.shape[:2]
 
+        if min_x != 999999 or max_x != 0 or min_y != 999999 or max_y != 0:
+            first_frame=False
+
         if Detect:
             #Take a vertical strip where the sproket should be (left hand side)
-            top_left_of_sproket_hole, bottom_right_of_sproket_hole, width_of_sproket_hole, height_of_sproket_hole, rotation, area, number_of_contours=detectSproket(image[0:h,0:int(w*0.22)], lower_t)
+            top_left_of_sproket_hole, bottom_right_of_sproket_hole, _, _, _, _, _=detectSproket(image[0:h,0:int(w*0.22)], lower_t)
 
         untouched_image=image.copy()
 
-        # draw actual detected sproket hole in grey
-        #cv.rectangle(image, top_left_of_sproket_hole, bottom_right_of_sproket_hole, (100,100,100), 3)
+        # draw actual detected sproket hole in blue
+        cv.rectangle(image, top_left_of_sproket_hole, bottom_right_of_sproket_hole, (255,255,100), 3)
 
         # Draw the box of recorded allowable TOP RIGHT positions (just for fun)
         if max_x>0:
-            cv.rectangle(image, (min_x,min_y), (max_x,max_y), (100,100,100), 3)
+            cv.rectangle(image, (min_x,min_y), (max_x,max_y), (100,255,100), 3)
 
         #Draw "average" size rectangle in red, based on detected hole
         tl=(bottom_right_of_sproket_hole[0]-average_width,top_left_of_sproket_hole[1])
         br=(tl[0]+average_width,tl[1]+average_height)
         #Top right
         tr=(br[0],tl[1])
-        #cv.rectangle(image, tl, br, (0,0,255), 3)
 
         draw_border(image,tl,br,(0,0,255),6,50,40)
 
-        #print(top_left_of_sproket_hole, bottom_right_of_sproket_hole,width_of_sproket_hole,height_of_sproket_hole, rotation, area, number_of_contours)
-
-        # Allowable tolerance around the "average"
-
-        # Frame dimensions - this will need to be altered on every scan
-        # perhaps enhance the GUI to use mouse coordinates?
-        # Negative offset X,Y and then W,H
-        # W and H even!
-        frame_dims=(-50,-450, 1786, 1200)
-
-        # right hand corner of sproket hole seems to be always best aligned (manual observation) so use that as datum for the whole frame capture
-        # calculate everything based on the ratio of the sproket holes
-        #frame_tl=(int(tr[0]-average_width*0.165) ,int(tr[1] - average_height*1.31))
-        frame_tl=(int(tr[0]+frame_dims[0]) ,int(tr[1] + frame_dims[1]))
-
-        # Height must be divisble by 2
-        #frame_br=(int(frame_tl[0]+ average_width*6.85),int(frame_tl[1]+ average_height*3.55))
-        frame_br=(int(frame_tl[0]+ frame_dims[2]),int(frame_tl[1]+ frame_dims[3]))
+        if init_frame_dims != None:
+            frame_dims=init_frame_dims
+            init_frame_dims=None
         
-        cv.rectangle(image, frame_tl, frame_br, (0,200,200), 8)
+        frame_tl=(int(tr[0]+frame_dims[0]) ,int(tr[1] + frame_dims[1]))
+        frame_br=(int(frame_tl[0]+ frame_dims[2]),int(frame_tl[1]+ frame_dims[3]))
 
+        cv.rectangle(image, frame_tl, frame_br, (0,200,200), 8)
         output_w= frame_br[0]-frame_tl[0]
         output_h= frame_br[1]-frame_tl[1]
 
-        #print(output_w,output_h)
-
         # Highlight top right
-        #cv.circle(image, (int(tr[0]), int(tr[1])), 8, (0, 0, 100), -1)
+        cv.circle(image, (int(tr[0]), int(tr[1])), 8, (0, 255, 0), -1)
         #padding=20
 
         if frame_tl[1]<0 or frame_tl[0]<0:
             print("frame_tl",frame_tl)
             manual_adjustment=True
-        #elif number_of_contours>40:
-        #    print("Contours",number_of_contours)
-        #    manual_adjustment=True
         elif tr[0]<min_x or tr[0]>max_x or tr[1]<min_y or tr[1]>max_y:
             print("Outside learned bounding box")
             manual_adjustment=True
-        #elif height_of_sproket_hole<(average_height-padding) or height_of_sproket_hole>(average_height+padding):
-        #    print("Sproket Height wrong!!",height_of_sproket_hole)
-        #    manual_adjustment=True
-        #elif width_of_sproket_hole<(average_width-padding) or width_of_sproket_hole>(average_width+padding):
-        #    print("Sproket width wrong!!",width_of_sproket_hole)
-        #    manual_adjustment=True
-        #elif top_left_of_sproket_hole[1]<590:
-        #    print("top_left_of_sproket_hole Y value low")
-        #    manual_adjustment=True
-        #elif top_left_of_sproket_hole[0]<80:
-        #    print("top_left_of_sproket_hole X value low")
-        #    manual_adjustment=True
 
         SMALL_STEP=2
         LARGE_STEP=10*SMALL_STEP
 
         if manual_adjustment==True:
             thumbnail=cv.resize(image, (0,0), fx=0.4, fy=0.4)
-            cv.putText(thumbnail, "Cursor keys adjust frame capture, SPACE to confirm", (0, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (200, 200, 200), 2, cv.LINE_AA)
-            cv.putText(thumbnail, "[ and ] adjust threshold, current value={0}".format(lower_t), (0, 60), cv.FONT_HERSHEY_SIMPLEX, 1, (200, 200, 200), 2, cv.LINE_AA)
+            cv.putText(thumbnail, "Cursor keys adjust frame capture(small step), SPACE to confirm", (0, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (200, 200, 200), 2, cv.LINE_AA)
+            cv.putText(thumbnail, "Numpad keys adjust frame capture(big step)", (0, 60), cv.FONT_HERSHEY_SIMPLEX, 1, (200, 200, 200), 2, cv.LINE_AA)
+            cv.putText(thumbnail, "[ and ] adjust threshold, current value={0}".format(lower_t), (0, 90), cv.FONT_HERSHEY_SIMPLEX, 1, (200, 200, 200), 2, cv.LINE_AA)
+            if first_frame==True:
+                cv.putText(thumbnail, "Use a,w,s,d or A,W,S,D to move the crop boundaries and +,- or *,/ to change it's size.", (0, 120), cv.FONT_HERSHEY_SIMPLEX, 1, (200, 200, 200), 2, cv.LINE_AA)
+                cv.putText(thumbnail, "Use q,e or Q,E to change it's aspect ratio.", (0, 150), cv.FONT_HERSHEY_SIMPLEX, 1, (200, 200, 200), 2, cv.LINE_AA)
             cv.imshow("Adjustment",thumbnail)
-            k = cv.waitKeyEx(0) 
-            #print("key",k)
+
+            k = cv.waitKeyEx(0)
+            # print("key",k)
 
             # Cursor UP
-            if k == 2490368:
+            if k == 2490368 or k == 63232:
+                print("up")
                 #Move sproket location up
                 # change Y coords
                 top_left_of_sproket_hole=(top_left_of_sproket_hole[0],top_left_of_sproket_hole[1]-SMALL_STEP)
                 bottom_right_of_sproket_hole=(bottom_right_of_sproket_hole[0],bottom_right_of_sproket_hole[1]-SMALL_STEP)
                 Detect=False
 
-            # Down
-            if k == 2621440:
+            # Cursor Down
+            if k == 2621440 or k == 63233:
+                print("down")
                 #Move sproket location down
                 # change Y coords
                 top_left_of_sproket_hole=(top_left_of_sproket_hole[0],top_left_of_sproket_hole[1]+SMALL_STEP)
                 bottom_right_of_sproket_hole=(bottom_right_of_sproket_hole[0],bottom_right_of_sproket_hole[1]+SMALL_STEP)
                 Detect=False
 
-            # left
-            if k == 2424832:
+            # Cursor Left
+            if k == 2424832 or k == 63234:
+                print("left")
                 #Move sproket location left
                 # change X coords
                 top_left_of_sproket_hole=(top_left_of_sproket_hole[0]-SMALL_STEP,top_left_of_sproket_hole[1])
                 bottom_right_of_sproket_hole=(bottom_right_of_sproket_hole[0]-SMALL_STEP,bottom_right_of_sproket_hole[1])
                 Detect=False
 
-            if k == 2555904:
+            # Cursor Right
+            if k == 2555904 or k == 63235:
+                print("right")
                 #Move sproket location right
                 # change X coords
                 top_left_of_sproket_hole=(top_left_of_sproket_hole[0]+SMALL_STEP,top_left_of_sproket_hole[1])
                 bottom_right_of_sproket_hole=(bottom_right_of_sproket_hole[0]+SMALL_STEP,bottom_right_of_sproket_hole[1])
                 Detect=False
 
-            #  8
+            #  8 (numpad up)
             if k == ord('8'):
+                print("upn")
                 #Move sproket location up
                 # change Y coords
                 top_left_of_sproket_hole=(top_left_of_sproket_hole[0],top_left_of_sproket_hole[1]-LARGE_STEP)
                 bottom_right_of_sproket_hole=(bottom_right_of_sproket_hole[0],bottom_right_of_sproket_hole[1]-LARGE_STEP)
                 Detect=False
 
-            # Down
+            # 2 (numpad down)
             if k == ord('2'):
+                print("downn")
                 #Move sproket location down
                 # change Y coords
                 top_left_of_sproket_hole=(top_left_of_sproket_hole[0],top_left_of_sproket_hole[1]+LARGE_STEP)
                 bottom_right_of_sproket_hole=(bottom_right_of_sproket_hole[0],bottom_right_of_sproket_hole[1]+LARGE_STEP)
                 Detect=False
 
-            # left
+            # 4 (numpad left)
             if k == ord('4'):
+                print("leftn")
                 #Move sproket location left
                 top_left_of_sproket_hole=(top_left_of_sproket_hole[0]-LARGE_STEP,top_left_of_sproket_hole[1])
                 bottom_right_of_sproket_hole=(bottom_right_of_sproket_hole[0]-LARGE_STEP,bottom_right_of_sproket_hole[1])
                 Detect=False
 
+            # 6 (numpad right)
             if k == ord('6'):
+                print("rightn")
                 #Move sproket location right
                 top_left_of_sproket_hole=(top_left_of_sproket_hole[0]+LARGE_STEP,top_left_of_sproket_hole[1])
                 bottom_right_of_sproket_hole=(bottom_right_of_sproket_hole[0]+LARGE_STEP,bottom_right_of_sproket_hole[1])
                 Detect=False
 
             if k == ord('r'):
+                print("Reset")
                 #Use previous frames locations
                 top_left_of_sproket_hole=previous_frame_top_left_of_sproket_hole
                 bottom_right_of_sproket_hole=previous_frame_bottom_right_of_sproket_hole
@@ -411,15 +408,88 @@ def processImage(original_image, average_width, average_height, average_area):
                 lower_t+=1
                 Detect=True
 
+            if first_frame == True:
+                if k == ord('-'):
+                    new_w, new_h, ratio = resize_with_aspect(frame_dims[2], frame_dims[3], -SMALL_STEP)
+                    frame_dims=(frame_dims[0],frame_dims[1]+(SMALL_STEP/2/ratio),new_w,new_h)
+                    Detect=False
+
+                if k == ord('+'):
+                    new_w, new_h, ratio = resize_with_aspect(frame_dims[2], frame_dims[3], SMALL_STEP)
+                    frame_dims=(frame_dims[0],frame_dims[1]-(SMALL_STEP/2/ratio),new_w,new_h)
+                    Detect=False
+
+                if k == ord('/'):
+                    new_w, new_h, ratio = resize_with_aspect(frame_dims[2], frame_dims[3], -LARGE_STEP)
+                    ratio = new_h / new_w
+                    frame_dims=(frame_dims[0],frame_dims[1]+(LARGE_STEP/2/ratio),new_w,new_h)
+                    Detect=False
+
+                if k == ord('*'):
+                    new_w, new_h, ratio = resize_with_aspect(frame_dims[2], frame_dims[3], LARGE_STEP)
+                    print(ratio, frame_dims[1])
+                    ratio = new_w / new_h
+                    frame_dims=(frame_dims[0],frame_dims[1]-(LARGE_STEP/2/ratio),new_w,new_h)
+                    Detect=False
+
+                if k == ord('w'):
+                    frame_dims=(frame_dims[0],frame_dims[1]-SMALL_STEP,frame_dims[2],frame_dims[3])
+                    Detect=False
+
+                if k == ord('s'):
+                    frame_dims=(frame_dims[0],frame_dims[1]+SMALL_STEP,frame_dims[2],frame_dims[3])
+                    Detect=False
+
+                if k == ord('a'):
+                    frame_dims=(frame_dims[0]-SMALL_STEP,frame_dims[1],frame_dims[2],frame_dims[3])
+                    Detect=False
+                
+                if k == ord('d'):
+                    frame_dims=(frame_dims[0]+SMALL_STEP,frame_dims[1],frame_dims[2],frame_dims[3])
+                    Detect=False
+
+                if k == ord('W'):
+                    frame_dims=(frame_dims[0],frame_dims[1]-LARGE_STEP,frame_dims[2],frame_dims[3])
+                    Detect=False
+
+                if k == ord('S'):
+                    frame_dims=(frame_dims[0],frame_dims[1]+LARGE_STEP,frame_dims[2],frame_dims[3])
+                    Detect=False
+
+                if k == ord('A'):
+                    frame_dims=(frame_dims[0]-LARGE_STEP,frame_dims[1],frame_dims[2],frame_dims[3])
+                    Detect=False
+                
+                if k == ord('D'):
+                    frame_dims=(frame_dims[0]+LARGE_STEP,frame_dims[1],frame_dims[2],frame_dims[3])
+                    Detect=False
+
+                if k == ord('q'):
+                    frame_dims=(frame_dims[0],frame_dims[1]+(SMALL_STEP/2),frame_dims[2],frame_dims[3]-SMALL_STEP)
+                    Detect=False
+
+                if k == ord('e'):
+                    frame_dims=(frame_dims[0],frame_dims[1]-(SMALL_STEP/2),frame_dims[2],frame_dims[3]+SMALL_STEP)
+                    Detect=False
+
+                if k == ord('Q'):
+                    frame_dims=(frame_dims[0],frame_dims[1]+(LARGE_STEP/2),frame_dims[2],frame_dims[3]-LARGE_STEP)
+                    Detect=False
+                
+                if k == ord('E'):
+                    frame_dims=(frame_dims[0],frame_dims[1]-(LARGE_STEP/2),frame_dims[2],frame_dims[3]+LARGE_STEP)
+                    Detect=False
+
+                print("frame_dims",frame_dims)
+
             if k == ord(' '):
                 #Accept
                 cv.destroyWindow("Adjustment")
                 manual_adjustment=False
 
         if manual_adjustment==False:
-
             #Black out the sproket hole
-            #cv.rectangle(untouched_image,(tr[0]+1,tr[1]-1),(tr[0]-2-average_width,tr[1]+2+average_height),color=(0,0,0),thickness=cv.FILLED)
+            # cv.rectangle(untouched_image,(tr[0]+1,tr[1]-1),(tr[0]-2-average_width,tr[1]+2+average_height),color=(0,0,0),thickness=cv.FILLED)
 
             if frame_tl[1]<0:
                 #Original image is smaller than the crop size/frame size, so pad out
@@ -523,7 +593,7 @@ try:
         else:
             print(filename)
 
-            new_image=processImage(img,  average_width, average_height, average_area)
+            new_image=processImage(img,  average_width, average_height, config.get('frame_dims'))
             h, w =new_image.shape[:2]
 
 
